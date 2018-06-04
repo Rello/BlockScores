2e3fa5ab28e60223e2e89e53b08bdaae19974091
Rello <github@scherello.de>
2018-06-04 22:41:11 +0200

    - abi.encodePacked() for keccak256 functions - inplemement migration functions

--- solidity/BlockScores.sol
+++ solidity/BlockScores.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.20;
 /// @author Marcel Scherello blockscores@scherello.de
 /// @notice Create a custom leaderboard and start counting the scores
 /// @dev All function calls are currently implement without side effects
+/// @dev V1.1.0
 contract BlockScores {
     struct Player {
         bytes32  playerName;
@@ -82,7 +83,7 @@ contract BlockScores {
     function addNewBoard(bytes32 name, string boardDescription) public payable returns(bytes32 boardHash){
         require(msg.value >= boardCost);
         balance += msg.value;
-        boardHash = keccak256(name, msg.sender);
+        boardHash = keccak256(abi.encodePacked(name, msg.sender));
         numBoards++;
         boards[boardHash] = Board(name, boardDescription, 0, msg.sender);
         emit newBoardCreated(boardHash);
@@ -93,7 +94,7 @@ contract BlockScores {
     /// @param admin The address of the admin address
     /// @return The possible hash of the leaderboard
     function createBoardHash(bytes32 name, address admin) pure public returns (bytes32){
-        return keccak256(name, admin);
+        return keccak256(abi.encodePacked(name, admin));
     }
 
     /// @notice Get the metadata of a leaderboard
@@ -101,20 +102,6 @@ contract BlockScores {
     /// @return Leaderboard name, description and number of players
     function getBoardByHash(bytes32 boardHash) constant public returns(bytes32,string,uint){
         return (boards[boardHash].boardName, boards[boardHash].boardDescription, boards[boardHash].numPlayers);
-    }
-
-    /// @notice Overwrite board metadata as contract owner only
-    /// @param boardHash The hash of the leaderboard to be modified
-    /// @param name The new name of the leaderboard
-    /// @param boardDescription The new subtitle for the leaderboard
-    /// @param boardOwner The address for the boardowner
-    /// @return true
-    function setBoardMetadata(bytes32 boardHash, bytes32 name, string boardDescription, uint8 numPlayers, address boardOwner) isOwner public returns(bool) {
-        boards[boardHash].boardName = name;
-        boards[boardHash].boardDescription = boardDescription;
-        boards[boardHash].numPlayers = numPlayers;
-        boards[boardHash].boardOwner = boardOwner;
-        return true;
     }
 
     /// @notice Overwrite leaderboard name and desctiption as owner only
@@ -180,7 +167,7 @@ contract BlockScores {
     function getPlayerId (bytes32 boardHash, bytes32 playerName, address playerAddress) constant internal returns (uint8) {
         Board storage g = boards[boardHash];
         for (uint8 i = 0; i <= g.numPlayers; i++) {
-            if ((keccak256(g.players[i].playerName) == keccak256(playerName) || playerAddress == g.players[i].playerAddress) && g.players[i].isActive == 1) {
+            if ((keccak256(abi.encodePacked(g.players[i].playerName)) == keccak256(abi.encodePacked(playerName)) || playerAddress == g.players[i].playerAddress) && g.players[i].isActive == 1) {
                 return i;
                 break;
             }
@@ -218,4 +205,53 @@ contract BlockScores {
         boards[boardHash].players[playerID].score_unconfirmed = 0;
         return true;
     }
+
+    /**
+    Migration Functions
+    */
+    /// @notice Read board metadata for migration as contract owner only
+    /// @param boardHash The hash of the leaderboard
+    /// @return Bord metadata
+    function migrationGetBoard(bytes32 boardHash) constant isOwner public returns(bytes32,string,uint,address) {
+        return (boards[boardHash].boardName, boards[boardHash].boardDescription, boards[boardHash].numPlayers, boards[boardHash].boardOwner);
+    }
+
+    /// @notice Write board metadata for migration as contract owner only
+    /// @param boardHash The hash of the leaderboard to be modified
+    /// @param name The new name of the leaderboard
+    /// @param boardDescription The new subtitle for the leaderboard
+    /// @param boardOwner The address for the boardowner
+    /// @return true
+    function migrationSetBoard(bytes32 boardHash, bytes32 name, string boardDescription, uint8 numPlayers, address boardOwner) isOwner public returns(bool) {
+        boards[boardHash].boardName = name;
+        boards[boardHash].boardDescription = boardDescription;
+        boards[boardHash].numPlayers = numPlayers;
+        boards[boardHash].boardOwner = boardOwner;
+        return true;
+    }
+
+    /// @notice Read player metadata for migration as contract owner
+    /// @param boardHash The hash of the leaderboard
+    /// @param playerID Index number of the player
+    /// @return Player metadata
+    function migrationGetPlayer(bytes32 boardHash, uint8 playerID) constant isOwner public returns (uint, bytes32, address, uint, uint, uint){
+        Player storage p = boards[boardHash].players[playerID];
+        return (playerID, p.playerName, p.playerAddress, p.score, p.score_unconfirmed, p.isActive);
+    }
+
+    /// @notice Write player metadata for migration as contract owner only
+    /// @param boardHash The hash of the leaderboard
+    /// @param playerID Player ID
+    /// @param playerName Player name
+    /// @param playerAddress Player address
+    /// @param score Player score
+    /// @param score_unconfirmed Player unconfirmed score
+    /// @param isActive Player isActive
+    /// @return true
+    function migrationSetPlayer(bytes32 boardHash, uint playerID, bytes32 playerName, address playerAddress, uint score, uint score_unconfirmed, uint isActive) isOwner public returns (bool) {
+        Board storage g = boards[boardHash];
+        g.players[playerID] = Player(playerName, playerAddress, score, score_unconfirmed, isActive);
+        return true;
+    }
+
 }
